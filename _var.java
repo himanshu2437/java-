3b1f790ea8f122a0ff4a2dbebdd57d4a4554b18e
Himanshu Kumar <himanshu.it.24037@recb.ac.in>
2026-04-23 23:45:39 +0530

    Add files via upload

---
+++ _var.java
@@ -0,0 +1,10 @@
+package Variable;
+
+public class _var {
+    public static void main (String[] args){
+        int a =10 ;
+        System.out.println(a);
+        String b = "Hello World! ";
+        System.out.println(b);
+    }
+}
