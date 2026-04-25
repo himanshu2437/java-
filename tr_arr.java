183dd9976807bb292c6a58f370e644b8c634b8b6
Himanshu Kumar <himanshu.it.24037@recb.ac.in>
2026-04-26 01:20:44 +0530

    Add files via upload

---
+++ tr_arr.java
@@ -0,0 +1,12 @@
+package ps;
+import java.util.Scanner;
+
+
+public class tr_arr {
+    public static void main (String[] args){
+        System.out.println("welcome to array print : ");
+        Scanner sc = new Scanner(System.in);
+        
+
+    }
+}
