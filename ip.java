4f0b00399cd4ea65c21a94490e5c119ddcff2908
Himanshu Kumar <himanshu.it.24037@recb.ac.in>
2026-04-27 10:30:08 +0530

    Add files via upload

---
+++ ip.java
@@ -0,0 +1,15 @@
+package ps;
+
+import java.util.Scanner;
+
+public class ip {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter your name : ");
+        String name = sc.nextLine(); // accept string twith the space
+        System.out.println("Good Morning " + name + " welcome to my program ");
+    }
+
+}
